17b6984bc079545d7ef9bb80cc66e82e09c3d48c
jamil <jamil.bce@gmail.com>
2024-04-27 22:53:05 +0500

    added straight line (compare line), set x and y labels and title of the plot.

--- gmtPlot.sh
+++ gmtPlot.sh
@@ -1,4 +1,7 @@
-gmt begin GMT_tut_7
+gmt begin 
 	gmt set GMT_THEME cookbook
-	gmt plot input.txt -R3/8/3/8 -Jx1i -B -Wthinner -Sc0.2c -Gpurple
+	gmt plot input.txt -R3/8/3/8 -Jx1i -B -Wthinner -Si0.2c -Gblue -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
+
+    # Plot the MsR=MsL line (y = x)
+    gmt plot compareLine.txt -R3/8/3/8 -Jx1i -B -Wthick  
 gmt end show
