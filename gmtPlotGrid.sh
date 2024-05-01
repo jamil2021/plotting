636aa7c7029949502fedfe276ab6c1fdd88f7e92
jamil <jamil.bce@gmail.com>
2024-05-01 21:30:00 +0500

    added gmtPlotGrid bash script using subplot command of GMT.

---
+++ gmtPlotGrid.sh
@@ -0,0 +1,16 @@
+gmt begin gmtPlotGrid pdf
+  gmt subplot begin 2x2 -Fs12c -M5p -A -Scb -Srl -R0/80/0/10
+    gmt subplot set
+    gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Gblue -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
+    gmt plot compareLine.txt -R3/8/3/8 -Jx2c -Wthick 
+    gmt subplot set
+    gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Gblue -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
+    gmt plot compareLine.txt -R3/8/3/8 -Jx2c -Wthick 
+    gmt subplot set
+    gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Gblue -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
+    gmt plot compareLine.txt -R3/8/3/8 -Jx2c -Wthick 
+    gmt subplot set
+    gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Gblue -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
+    gmt plot compareLine.txt -R3/8/3/8 -Jx2c -Wthick 
+  gmt subplot end
+gmt end show
