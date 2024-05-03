13108c06e23177ecc5080bc781597a581e30c25f
jamil <jamil.bce@gmail.com>
2024-05-03 07:51:57 +0500

    changed colours.

--- gmtPlotGrid.sh
+++ gmtPlotGrid.sh
@@ -4,13 +4,13 @@ gmt begin gmtPlotGrid pdf
     gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Gblue -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
     gmt plot compareLine.txt -R3/8/3/8 -Jx2c -Wthick 
     gmt subplot set
-    gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Gblue -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
+    gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Gred -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
     gmt plot compareLine.txt -R3/8/3/8 -Jx2c -Wthick 
     gmt subplot set
-    gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Gblue -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
+    gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Ggreen -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
     gmt plot compareLine.txt -R3/8/3/8 -Jx2c -Wthick 
     gmt subplot set
-    gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Gblue -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
+    gmt plot input.txt -R3/8/3/8 -Jx2c -Wthinner -Si0.2c -Gyellow -BWS+t"MsL vs MsR" -Bx+l"MsL" -By+l"MsR"
     gmt plot compareLine.txt -R3/8/3/8 -Jx2c -Wthick 
   gmt subplot end
 gmt end show
