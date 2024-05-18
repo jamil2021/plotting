67bfab8204c8e42f8fa6737a2eae44d7dfd94652
jamil <jamil.bce@gmail.com>
2024-05-18 18:21:58 +0500

    map of pakistan and surrounding region using pygmt.

---
+++ pygmtPlotPakRegion.py
@@ -0,0 +1,22 @@
+import pygmt
+
+# Define the region to plot (Pakistan and surrounding areas)
+region = [60, 80, 20, 40]  # [xmin, xmax, ymin, ymax]
+
+# Create a figure
+fig = pygmt.Figure()
+
+# Add a basemap
+fig.basemap(region=region, projection="M6i", frame=True)
+
+# Add coastlines
+fig.coast(shorelines=True, borders=[1, 2], water="skyblue", land="lightgray")
+
+# Add a title
+fig.text(text="Map of Pakistan and Surrounding Regions", x=70, y=38, font="16p,Helvetica-Bold", justify="CM")
+
+# Show the figure
+fig.show()
+
+# Save the figure
+fig.savefig("pakistan_map.png")
